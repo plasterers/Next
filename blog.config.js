166170a347759e20d390959895119583283fbdad
plasterers <165124377+plasterers@users.noreply.github.com>
2024-03-31 00:36:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,14 +16,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个工程人, Hi，我是一个打工人,Hi，我是一个搬砖人,欢迎来建筑知识库的博客🎉',
+    '',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '泥瓦匠', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的工程人🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://dachengshan.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '建筑','建造师','施工方案','施工组织设计','项目管理','工程资料',// 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '建筑',// 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'tangshan.team@foxmail.com', // 邮箱地址 例如mail@tangly1024.com
